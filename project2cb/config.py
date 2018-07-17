270c179f9e6a15eed44baefe308ff3cbc481179f
CharlesBreedlove <34756041+CharlesBreedlove@users.noreply.github.com>
2018-07-16 19:10:33 -0500

    Add files via upload

---
+++ project2cb/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "tyCZmofxklG6DcjWnRCRpdhXK"
+consumer_secret = "GSmAaTuRGSB2yhTqyHbUBufL4oVGAw3uVueRYkeNTzuTsoopKa"
+access_token = "986021521310314496-ZrCHbr4I9892w5zTJ4MAP0j0T9M5ZLB"
+access_token_secret = "Hr30Noc2KNnUVme1nMlSnrYWqVOCqgDz8GtSAM2f95qsi"
